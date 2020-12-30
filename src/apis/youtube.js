2ab435c3a39d779237e9d48b292619d9763eeef8
mike <mikemcfaddin@gmail.com>
2020-12-30 09:30:50 -0500

    most components are working and connection to youtube api is working

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
-const KEY = process.env.YOUTUBE_KEY
+import axios from 'axios';
+// const KEY = process.env.REACT_APP_YOUTUBE_KEY
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: ''
+  }
+});
